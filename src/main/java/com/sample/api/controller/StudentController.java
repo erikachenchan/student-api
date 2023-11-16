03fda9215123874fad5d899b2650ee156b0c0bbc
erikachan <ahelp164@gmail.com>
2023-11-16 13:07:43 +0000

    Added GetMapping annotation

--- src/main/java/com/sample/api/controller/StudentController.java
+++ src/main/java/com/sample/api/controller/StudentController.java
@@ -2,12 +2,14 @@ package com.sample.api.controller;
 
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class StudentController {
+    @GetMapping
     public String helloWorld() {
         return "Hello World";
     }
