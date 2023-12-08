fda7b653d712959972db0c63962858980e62a984
erikachan <ahelp164@gmail.com>
2023-12-08 04:08:47 +0000

    Added @PathVariable annotation for params and implemented good practices of naming apis and its routes

--- src/main/java/com/sample/api/controller/StudentController.java
+++ src/main/java/com/sample/api/controller/StudentController.java
@@ -6,8 +6,8 @@ import com.sample.api.services.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +17,7 @@ import java.util.List;
  * @RestController - exposing an endpoint starts here and allows Controller to handle the path
  */
 @RestController
-@RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
-@CrossOrigin(origins = {"http://localhost:8081",}, maxAge = 3000)
+@RequestMapping(value = "/api/students", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class StudentController {
 
@@ -26,9 +25,25 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
-
-    @GetMapping("/students")
+    @GetMapping("/")
     public List<StudentHouse> getStudent() {
+        return this.studentService.getAllStudent();
+    }
+
+    // Pageable
+//    @GetMapping("/students")
+//    public List<StudentHouse> getStudent(Pageable page) {
+//        System.out.println("page=" + page.getNumberOfPages());
+//        return this.studentService.getAllStudent();
+//    }
+
+
+    /**
+     * if you want to accept parameter in this method
+     * https://www.baeldung.com/spring-requestmapping
+     */
+    @GetMapping("/{name}")
+    public List<StudentHouse> getStudentHouseByName(@PathVariable String name) {
 
         return this.studentService.getAllStudent();
     }
